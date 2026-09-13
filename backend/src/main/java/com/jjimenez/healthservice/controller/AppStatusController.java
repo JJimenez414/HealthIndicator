ab8a2df2a8aa8d85ee35733c09842c68400c1ee4
jose jimenez <josejimenez@joses-MacBook-Pro.local>
2026-09-13 12:51:10 -0500

    app status controller implementation done

--- backend/src/main/java/com/jjimenez/healthservice/controller/AppStatusController.java
+++ backend/src/main/java/com/jjimenez/healthservice/controller/AppStatusController.java
@@ -1,5 +1,31 @@
 package com.jjimenez.healthservice.controller;
 
-// TODO: expose GET /api/apps -> List<AppStatus>
+import com.jjimenez.healthservice.service.AppStatusService;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.jjimenez.healthservice.model.AppStatusDTO;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.IOException;
+
+@RestController
 public class AppStatusController {
+
+    AppStatusService appStatus;
+
+    public AppStatusController(AppStatusService appStatus) {
+        this.appStatus = appStatus;
+    }
+
+    @GetMapping("/api/apps")
+    public ResponseEntity<?> getStatus() {
+
+        try {
+            return ResponseEntity.ok(appStatus.getAppStatuses());
+        } catch (IOException | InterruptedException e) {
+            return ResponseEntity.internalServerError().body("Failed to check app statuses: " + e.getMessage());
+        }
+
+    }
 }
